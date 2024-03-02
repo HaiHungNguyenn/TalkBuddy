5ca4338189a1fe39b621969598123d2aade36897
supernhilty <nhilty302814@gmail.com>
2024-03-02 22:14:28 +0700

    [Nhi] update ClientChatBox - add NickName which default is client name

--- TalkBuddy.Domain/Entities/ClientChatBox.cs
+++ TalkBuddy.Domain/Entities/ClientChatBox.cs
@@ -1,13 +1,13 @@
 ﻿using TalkBuddy.Domain.Entities.BaseEntities;
 
 namespace TalkBuddy.Domain.Entities;
-
 public partial class ClientChatBox : BaseEntity<Guid>
 {
     public bool IsBlocked { get; set; }
     public bool IsLeft { get; set; }
     public bool IsNotificationOn { get; set; }
     public bool IsModerator { get; set; }
+    public string? NickName { get; set; }
     public Guid ClientId { get; set; }
     public Guid ChatBoxId { get; set; }
     public virtual ChatBox ChatBox { get; set; } = null!;
