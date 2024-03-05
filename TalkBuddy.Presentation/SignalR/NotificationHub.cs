f27c19fd47642b4cb5c645e540c7e16f8e8dea5b
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-03-05 11:43:34 +0700

    [Notification][Hai] migrate db and basics class for notificatin

---
+++ TalkBuddy.Presentation/SignalR/NotificationHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TalkBuddy.Presentation.SignalR;
+
+public class NotificationHub : Hub
+{
+    
+}
---
+++ TalkBuddy.Service/Interfaces/INotificationService.cs
@@ -0,0 +1,9 @@
+﻿using TalkBuddy.Domain.Entities;
+
+namespace TalkBuddy.Service.Interfaces;
+
+public interface INotificationService
+{
+    Task CreateNotification(Notification notification);
+    Task<IQueryable<Notification>> GetNotificationByClient(Guid clientId);
+}
