3f6edbe08e62ea6993ad7363d9919c47972ac0d4
duykasama <duykasama@gmail.com>
2024-03-10 20:22:54 +0700

    [Duy] Display username on hover

--- TalkBuddy.Presentation/wwwroot/js/site.js
+++ TalkBuddy.Presentation/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
+console.log(uuidv4());
