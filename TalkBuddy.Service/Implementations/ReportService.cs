de94ec9b565b14231ced5db6b601159b6ec127fa
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-03-15 19:41:10 +0700

    [Moderator][Hai] Add ban user feature

--- TalkBuddy.Service/Implementations/ReportService.cs
+++ TalkBuddy.Service/Implementations/ReportService.cs
@@ -1,4 +1,5 @@
-﻿using TalkBuddy.Common.Enums;
+﻿using Microsoft.EntityFrameworkCore;
+using TalkBuddy.Common.Enums;
 using TalkBuddy.DAL.Interfaces;
 using TalkBuddy.Domain.Entities;
 using TalkBuddy.Service.Interfaces;
@@ -9,11 +10,13 @@ public class ReportService : IReportService
 {
     private readonly IReportRepository _reportRepository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IClientRepository _clientRepository;
 
-    public ReportService(IReportRepository reportRepository,IUnitOfWork unitOfWork)
+    public ReportService(IReportRepository reportRepository,IUnitOfWork unitOfWork, IClientRepository clientRepository)
     {
         _reportRepository = reportRepository;
         _unitOfWork = unitOfWork;
+        _clientRepository = clientRepository;
     }
     
     public async Task CreateReport(Report report)
@@ -30,11 +33,32 @@ public class ReportService : IReportService
         await _unitOfWork.CommitAsync();
     }
 
-    public async Task BanUser(Guid reportId)
+    public async Task BanUser(Guid reportId, SuspensionType suspensionType)
     {
         var report = await _reportRepository.GetAsync(x => x.Id == reportId) ?? throw new Exception($"Not Found Report: {reportId}");
         report.Status = ReportationStatus.RESOLVED;
+        var client = await _clientRepository.GetAsync(x => x.Id == report.ReportedClientId);
+        client.IsAccountSuspended = true;
+        client.SuspensionCount++;
+        client.SuspensionEndDate = suspensionType switch
+        {
+            SuspensionType.SIXHOUR => client.SuspensionEndDate.AddHours(6),
+            SuspensionType.ONEDAY => client.SuspensionEndDate.AddDays(1),
+            SuspensionType.THREEDAY => client.SuspensionEndDate.AddDays(3),
+            SuspensionType.ONEMONTH => client.SuspensionEndDate.AddMonths(1),
+            SuspensionType.SIXMONTH => client.SuspensionEndDate.AddMonths(6),
+            _ => client.SuspensionEndDate 
+        };
+        await _clientRepository.UpdateAsync(client);
         await _reportRepository.UpdateAsync(report);
         await _unitOfWork.CommitAsync();
+    }
+
+    public async Task<IQueryable> GetSuspendedClient()
+    {
+        var clients =(await _clientRepository.FindAsync(x => x.IsAccountSuspended == true && x.SuspensionEndDate < DateTime.Now))
+                .Include(x => x.InformantClients)
+                .Include(x => x.ReportedClients);
+        return clients;
     }
 }
--- TalkBuddy.Service/Interfaces/IReportService.cs
+++ TalkBuddy.Service/Interfaces/IReportService.cs
@@ -1,4 +1,5 @@
-﻿using TalkBuddy.Domain.Entities;
+﻿using TalkBuddy.Common.Enums;
+using TalkBuddy.Domain.Entities;
 
 namespace TalkBuddy.Service.Interfaces;
 
@@ -7,5 +8,7 @@ public interface IReportService
     Task CreateReport(Report report);
 
     Task DismissReport(Guid reportId);
-    Task BanUser(Guid reportId);
+    Task BanUser(Guid reportId, SuspensionType suspensionType);
+
+    Task<IQueryable> GetSuspendedClient();
 }
